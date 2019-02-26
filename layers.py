b701ce75bba6ed7a85641341f7c1a33d1b0db068
Daniel <dfdazac@gmail.com>
2019-02-26 12:33:34 +0100

    First draft

---
+++ layers.py
@@ -0,0 +1,98 @@
+import torch
+import torch.nn as nn
+
+# Adapted from https://github.com/gpeyre/SinkhornAutoDiff
+class SinkhornDistance(nn.Module):
+    r"""
+    Given two empirical measures with n points each with locations x and y,
+    outputs an approximation of the regularized OT cost for point clouds.
+
+    Args:
+        eps (float): regularization coefficient
+        max_iter (int): maximum number of Sinkhorn iterations
+        reduction (string, optional): Specifies the reduction to apply to the output:
+            'none' | 'mean' | 'sum'. 'none': no reduction will be applied,
+            'mean': the sum of the output will be divided by the number of
+            elements in the output, 'sum': the output will be summed. Default: 'mean'
+
+    Shape:
+        - Input: :math:`(N, \text{in\_features})`, :math:`(N, \text{in\_features})`
+        - Output: :math:`(N)` or :math:`()`, depending on `reduction`
+    """
+    def __init__(self, eps, max_iter, reduction='mean'):
+        super(SinkhornDistance, self).__init__()
+        self.eps = eps
+        self.max_iter = max_iter
+        self.reduction = reduction
+
+    def forward(self, x, y):
+        # The Sinkhorn algorithm takes as input three variables :
+        C = self._cost_matrix(x, y)  # Wasserstein cost function
+        n_points = x.shape[-2]
+        if x.dim() == 2:
+            batch_size = 1
+        else:
+            batch_size = x.shape[0]
+
+        # both marginals are fixed with equal weights
+        mu = torch.empty(batch_size, n_points, dtype=torch.float,
+                         requires_grad=False).fill_(1.0 / n_points).squeeze()
+        nu = torch.empty(batch_size, n_points, dtype=torch.float,
+                         requires_grad=False).fill_(1.0 / n_points).squeeze()
+
+        u = torch.zeros_like(mu)
+        v = torch.zeros_like(nu)
+        # To check if algorithm terminates because of threshold
+        # or max iterations reached
+        actual_nits = 0
+        # Stopping criterion
+        thresh = 1e-1
+
+        # Sinkhorn iterations
+        for i in range(self.max_iter):
+            u1 = u  # useful to check the update
+            u = self.eps * (torch.log(mu) - self.lse(self.M(C, u, v))) + u
+            v = self.eps * (torch.log(nu) - self.lse(self.M(C, u, v).transpose(-2, -1))) + v
+            err = (u - u1).abs().sum(-1).mean()
+
+            actual_nits += 1
+            if err.item() < thresh:
+                break
+
+        U, V = u, v
+        # Transport plan pi = diag(a)*K*diag(b)
+        pi = torch.exp(self.M(C, U, V))
+        # Sinkhorn distance
+        cost = torch.sum(pi * C, dim=(-2, -1))
+
+        if self.reduction == 'mean':
+            cost = cost.mean()
+        elif self.reduction == 'sum':
+            cost = cost.sum()
+
+        return cost, pi, C
+
+    def M(self, C, u, v):
+        "Modified cost for logarithmic updates"
+        "$M_{ij} = (-c_{ij} + u_i + v_j) / \epsilon$"
+        return (-C + u.unsqueeze(-1) + v.unsqueeze(-2)) / self.eps
+
+    @staticmethod
+    def _cost_matrix(x, y, p=2):
+        "Returns the matrix of $|x_i-y_j|^p$."
+        x_col = x.unsqueeze(-2)
+        y_lin = y.unsqueeze(-3)
+        C = torch.sum((torch.abs(x_col - y_lin)) ** p, -1)
+        return C
+
+    @staticmethod
+    def lse(A):
+        "log-sum-exp"
+        # add 10^-6 to prevent NaN
+        result = torch.log(torch.exp(A).sum(-1) + 1e-6)
+        return result
+
+    @staticmethod
+    def ave(u, u1, tau):
+        "Barycenter subroutine, used by kinetic acceleration through extrapolation."
+        return tau * u + (1 - tau) * u1
